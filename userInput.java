a7bfd387b296dc744d5f253ed95228e4bd777731
Zunmood <66582305+Zunmood@users.noreply.github.com>
2020-08-22 17:23:23 +0530

    Update userInput.java

--- userInput.java
+++ userInput.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class userInput {
     public static void main(String[] args) {
-        Scanner scan = new Scanner (System.in);
+        Scanner sc = new Scanner (System.in);
         System.out.println("Enter a number");
-        int enteredNumber = scan.nextInt();
+        int enteredNumber = sc.nextInt();
         System.out.println("The entered number is " + enteredNumber);
         System.out.println("Enter a number");
-        Double enteredDouble = scan.nextDouble();
+        Double enteredDouble = sc.nextDouble();
         System.out.println("The entered number is " + enteredDouble);
         System.out.println("What is your name");
-        String enteredString = scan.next();
+        String enteredString = sc.next();
         System.out.println("My name is  " + enteredString);
     }
 }
